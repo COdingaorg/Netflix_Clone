3174f7a9e66e53af26b2327dedaa20c30ac5f2f8
Caleb Odinga <calemasanga@gmail.com>
2021-07-09 17:56:30 +0300

    adds database to application

--- netflix_clone/settings.py
+++ netflix_clone/settings.py
@@ -81,8 +81,10 @@ WSGI_APPLICATION = 'netflix_clone.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.posgresql',
+        'NAME': 'netflix_clone',
+        'USER':config('moringa'),
+        'USER_PASSWORD':config('access'),
     }
 }
 
