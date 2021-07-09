eba6712bad748d6a3b710b0a9921225779571e68
Caleb Odinga <calemasanga@gmail.com>
2021-07-09 18:49:51 +0300

    adds django registration, login and logout in urls

--- netflix/urls.py
+++ netflix/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import url
 from netflix import views
 
 urlpatterns = [
-  url(r'^$', views.index, name = 'home'),
+  url(r'', views.index, name = 'home'),
   url(r'search_results/$', views.search_movies, name = 'search_movies'),
+  url(r'', views.login, name = 'login'),
+  url(r'logout', views.logout, name = 'logout'),
   ]
