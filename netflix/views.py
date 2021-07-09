f423b2b511d0fdc97f7e6ec95ccd5a2f072e01ff
Caleb Odinga <calemasanga@gmail.com>
2021-07-09 16:11:57 +0300

    adds cards to display popolar movies on index, add popular movies function in index view

--- netflix/views.py
+++ netflix/views.py
@@ -7,35 +7,24 @@ def index(request):
   title = 'Netflix clone Homepage'
   results_returned = {}
 
-  link = settings.API_LINK
+  link = 'https://api.themoviedb.org/3/{type}/{search_term}?api_key={api_key}&language=en-US&page=1'
   apikey = settings.API_KEY
   url = link.format(type = 'movie', search_term = 'popular', api_key = apikey)
   movie_results = requests.get(url)
-  if movie_results.status_code==200:
-    results_returned = movie_results.json()
-    results_returned['success'] = True
-  else:
-    results_returned['success'] = False
-    if results_returned.status_code == 400:
-      results_returned['message'] = 'No Movies Found'
-    
-    else:
-      results_returned['message'] = 'Movies are Not available at the moment'
-  
-    movie_title = results_returned['original_title']
-    context = {
-      'results':results_returned,
-      'movie_title':movie_title,
+  results_returned = movie_results.json()
+  results_returned['success'] = True
+  context = {
+      'results':results_returned['results'],
       'title':title
     }
 
-    return render(request, 'index.html', context)
+  return render(request, 'index.html', context)
 
 def search_movies(request):
   
   if 'search_movie' in request.GET and request.GET['search_movie']:
     search_trm= request.GET.get('search_movie')
-    link = settings.API_LINK
+    link = 'https://api.themoviedb.org/3/{type}/{search_term}?api_key={api_key}&language=en-US&page=1'
     apikey = settings.API_KEY
     url = link.format(type = 'movie', search_term = search_trm, api_key = apikey)
     results = requests.get(url)
