315a05a82112315effcf8c255c82db2756195b73
Caleb Odinga <calemasanga@gmail.com>
2021-07-09 17:39:09 +0300

    adds footer and styles it

--- netflix/views.py
+++ netflix/views.py
@@ -5,17 +5,17 @@ import requests
 # Create your views here.
 def index(request):
   title = 'Netflix clone Homepage'
-  results_returned = {}
+  url = (settings.API_LINK).format(type = 'movie', search_term = 'popular', api_key = settings.API_KEY)
+  results_returned = requests.get(url).json()
 
-  link = 'https://api.themoviedb.org/3/{type}/{search_term}?api_key={api_key}&language=en-US&page=1'
-  apikey = settings.API_KEY
-  url = link.format(type = 'movie', search_term = 'popular', api_key = apikey)
-  movie_results = requests.get(url)
-  results_returned = movie_results.json()
-  results_returned['success'] = True
+  #returning now playing category
+  url = (settings.API_LINK).format(type = 'movie', search_term = 'top_rated', api_key = settings.API_KEY)
+  results_returned2 = requests.get(url).json()
+
   context = {
       'results':results_returned['results'][0:12],
-      'title':title
+      'top_rated':results_returned2['results'][0:6],
+      'title':title,
     }
 
   return render(request, 'index.html', context)
@@ -24,12 +24,11 @@ def search_movies(request):
   
   if 'search_movie' in request.GET and request.GET['search_movie']:
     search_trm= request.GET.get('search_movie')
-    link = 'https://api.themoviedb.org/3/{type}/{search_term}?api_key={api_key}&language=en-US&page=1'
+    link = settings.API_LINK
     apikey = settings.API_KEY
     url = link.format(type = 'movie', search_term = search_trm, api_key = apikey)
     results = requests.get(url)
     results_returned = results.json()
-    print(url)
     title = results_returned['original_title']
     context = {
       'results':results_returned,
