0fd5463bd4993513d212b88ac4c6176cc49edb9c
Caleb Odinga <calemasanga@gmail.com>
2021-07-21 22:08:44 +0300

    add user_profile model and pillow for images

--- netflix/models.py
+++ netflix/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.deletion import CASCADE
 
 # Create your models here.
+class UserProfile(models.Model):
+  username = models.CharField(max_length=100)
+  tagline = models.CharField(max_length=250)
+  photo_path = models.ImageField(upload_to = 'profiles/')
+  user = models.ForeignKey(User, on_delete=CASCADE )
+
--- netflix_clone/settings.py
+++ netflix_clone/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-x)5dlaab^9e9rz=_18al0!nc9-6dvgdzc@n)2d^wwofx29_#$!'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -68,6 +68,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.contrib.messages.context_processors.media',
             ],
         },
     },
